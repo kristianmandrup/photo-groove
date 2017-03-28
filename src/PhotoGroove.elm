3a9de4835f208c3515816a70397045932ffcf617
Kristian Mandrup <kmandrup@gmail.com>
2017-03-28 11:37:11 +0200

    user select photo added

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,6 +2,7 @@ module PhotoGroove exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 urlPrefix : String
@@ -9,21 +10,22 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
-viewThumbnail : String -> { a | url : String } -> Html msg
+viewThumbnail : String -> { a | url : String } -> Html { data : String, operation : String }
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
         ]
         []
 
 
-view : { photos : List { url : String }, selectedUrl : String } -> Html msg
+view : { photos : List { url : String }, selectedUrl : String } -> Html { data : String, operation : String }
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            (List.map (\photo -> viewThumbnail model.selectedUrl photo)
+            (List.map (viewThumbnail model.selectedUrl)
                 model.photos
             )
         , img
@@ -45,6 +47,21 @@ initialModel =
     }
 
 
-main : Html msg
+update :
+    { b | data : a, operation : String }
+    -> { c | selectedUrl : a }
+    -> { c | selectedUrl : a }
+update msg model =
+    if msg.operation == "SELECT_PHOTO" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
+
+main : Program Never { photos : List { url : String }, selectedUrl : String } { data : String, operation : String }
 main =
-    view initialModel
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
